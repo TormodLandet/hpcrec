e40dfac04bcb1d0d6796b1660c75d1c28811b263
Tormod Landet <tormod@landet.net>
2016-02-10 10:55:25 +0100

    Support extending the nonzero structure of the assembled matrix

--- hpc/assembly.py
+++ hpc/assembly.py
@@ -4,7 +4,7 @@ from .polynomials import eval_phi
 import numpy
 
 
-def assemble(domain, method='csr'):
+def assemble(domain, method='csr', additional_connections=None):
     """
     Assemble HPC matrix for the given domain
     """
@@ -19,10 +19,17 @@ def assemble(domain, method='csr'):
             neighbours, coeffs, _, _ = eval_phi(domain, dof)
             tmp = zip(neighbours, coeffs)
             tmp.append((dof, -1))
+            
+            if additional_connections is not None:
+                for d in additional_connections[dof]:
+                    assert d not in neighbours
+                    tmp.append((d, 0))
+            
             tmp.sort()
             data.extend(-c for _d, c in tmp)
             indices.extend(d for d, _c in tmp)
             indptr.append(indptr[-1] + len(tmp))
+        
         data = numpy.array(data, numpy.float64)
         indices = numpy.array(indices, numpy.int32)
         indptr = numpy.array(indptr, numpy.int32)
@@ -38,6 +45,12 @@ def assemble(domain, method='csr'):
             for i, dof_i in enumerate(neighbours):
                 A[dof, dof_i] = -coeffs[i]
             A[dof,dof] = 1
+        
+        if additional_connections is not None:
+            for dof, cons in enumerate(additional_connections):
+                for d in cons:
+                    assert A[dof, d] == 0
+                    A[dof, d] = 0
     
     b = Vector(N)
     return A, b
