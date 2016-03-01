b19eabc0949478d3cab8840fd3d0f041cb487621
Tormod Landet <tormod@landet.net>
2016-03-01 15:41:28 +0100

    Cylinder: comparative logfile plotting

--- demos/cylinder_post.py
+++ demos/cylinder_post.py
@@ -1,17 +1,5 @@
-import sys
-import argparse
 import numpy
 from matplotlib import pyplot
-
-parser = argparse.ArgumentParser()
-parser.add_argument('logfile', default='cylinder.log')
-parser.add_argument('--tstart', type=float, default=0, help='steady state start')
-args = parser.parse_args()
-
-
-pyplot.style.use('ggplot')
-tstart = args.tstart
-inpfile = args.logfile
 
 
 def ts_zero_upcrossing_period(t, I):
@@ -35,46 +23,61 @@ def ts_zero_upcrossings(ts):
     return numpy.array(I, int)
 
 
-def main():
-    global inpfile
-    if sys.argv[1:]:
-        inpfile = sys.argv[1]
-    
-    timeseries = {'t': [], 'Fp0': [], 'Fp1': [], 'Fv0': [], 'Fv1': []}
-    inp = {}
-    for line in open(inpfile, 'rt'):
-        if line.startswith('    ') and line.count('=') == 1:
-            wds = line.split()
-            name = wds[0]
-            K = 4 + len(name) + 3
-            value_str = line[K:].strip()
-            value = eval(value_str)
-            inp[name] = value
-            continue
-        elif 'Fv' not in line or line[-1] != '\n':
-            continue
-        wds = line.split()
-        ip = wds.index('Fp:')
-        iv = wds.index('Fv:')
+class LogfileReader(object):
+    def __init__(self, filename):
+        timeseries = {'t': [], 'Fp0': [], 'Fp1': [], 'Fv0': [], 'Fv1': []}
+        inp = {}
         
-        timeseries['t'].append(float(wds[3]))
-        timeseries['Fp0'].append(float(wds[ip+1]))
-        timeseries['Fp1'].append(float(wds[ip+2]))
-        timeseries['Fv0'].append(float(wds[iv+1]))
-        timeseries['Fv1'].append(float(wds[iv+2]))
+        # Read data from logfile
+        for line in open(filename, 'rt'):
+            if line.startswith('    ') and line.count('=') == 1:
+                wds = line.split()
+                name = wds[0]
+                K = 4 + len(name) + 3
+                value_str = line[K:].strip()
+                value = eval(value_str)
+                inp[name] = value
+                continue
+            elif 'Fv' not in line or line[-1] != '\n':
+                continue
+            wds = line.split()
+            ip = wds.index('Fp:')
+            iv = wds.index('Fv:')
+            
+            timeseries['t'].append(float(wds[3]))
+            timeseries['Fp0'].append(float(wds[ip+1]))
+            timeseries['Fp1'].append(float(wds[ip+2]))
+            timeseries['Fv0'].append(float(wds[iv+1]))
+            timeseries['Fv1'].append(float(wds[iv+2]))
+            
+        # Make arrays
+        for name in 't Fp0 Fp1 Fv0 Fv1'.split():
+            timeseries[name] = numpy.array(timeseries[name], float)
+        
+        self.input = inp
+        self.timeseries = timeseries
+
+
+def plot_logfile(inpfile, tstart):
+    # Read the log
+    log = LogfileReader(inpfile)
+    inp = log.input
+    timeseries = log.timeseries
     
+    # Get parameters
     d = inp.get('d', 0.1)
     U0 = inp.get('U0', 0.1)
     rho = inp.get('rho', 1)
-    td1, _td2, td3 = inp.get('disturbance_time', (0, 0, 0))
+    #td1, _td2, td3 = inp.get('disturbance_time', (0, 0, 0))
+    td1 = td3 = 0
     
-    for name in 't Fp0 Fp1 Fv0 Fv1'.split():
-        timeseries[name] = numpy.array(timeseries[name], float)
+    # Filter the time series
     t = timeseries['t']
     sieve = (t > tstart) & ((t < td1) | (t > td3))
     t =  t[sieve]
     
-    figures = [pyplot.figure(), pyplot.figure()]
+    fig = pyplot.figure()
+    axes = [fig.add_subplot(n) for n in (211, 212)]
     legends = [[], []]
     
     print 'Input:'
@@ -89,8 +92,7 @@ def main():
         scale = 2/(rho*U0**2*d)
         
         direction = int(name[-1])
-        fig = figures[direction]
-        pyplot.sca(fig.gca())
+        pyplot.sca(axes[direction])
         
         data_plot = data2
         line, = pyplot.plot(t, data_plot*scale)
@@ -111,14 +113,93 @@ def main():
             pyplot.plot(t[I], data_plot[I]*scale, 'bo')
             pyplot.plot(t[J], data_plot[J]*scale, 'ro')
     
-    for fig, leg in zip(figures, legends):
-        pyplot.sca(fig.gca())
+    for ax, leg in zip(axes, legends):
+        pyplot.sca(ax)
         lines, names = zip(*leg)
         pyplot.legend(lines, names)
+    pyplot.tight_layout()
     
     print 'Tmax', t[-1]
     pyplot.show()
 
 
+def plot_multiple(args, tstart):
+    name = args[-1]
+    assert name in 'Fp0 Fp1 Fv0 Fv1'.split()
+    
+    fig = pyplot.figure()
+    ax = fig.add_subplot(111)
+    legends = []
+    
+    for inpfile in args[:-1]:
+        # Read the log
+        log = LogfileReader(inpfile)
+        inp = log.input
+        timeseries = log.timeseries
+    
+        # Get parameters
+        d = inp.get('d', 0.1)
+        U0 = inp.get('U0', 0.1)
+        rho = inp.get('rho', 1)
+        #td1, _td2, td3 = inp.get('disturbance_time', (0, 0, 0))
+        td1 = td3 = 0
+    
+        # Filter the time series
+        t = timeseries['t']
+        sieve = (t > tstart) & ((t < td1) | (t > td3))
+        t =  t[sieve]
+        
+        data = timeseries[name][sieve]
+        mean = data.mean()
+        data2 = data - mean
+        I = ts_zero_upcrossings(data2)
+        scale = 2/(rho*U0**2*d)
+        
+        direction = int(name[-1])
+        pyplot.sca(ax)
+        
+        data_plot = data
+        line, = pyplot.plot(t, data_plot*scale)
+        legends.append((line, inpfile))
+        pyplot.title('Force in %s direction (%s)' % ('lift' if direction else 'drag', name))
+        
+        if len(I) > 2:
+            J = ts_peaks(data2, I)
+            peaks = data[J]
+            Tz = ts_zero_upcrossing_period(t, I)
+            a = peaks.mean()    
+            print inpfile, name
+            print '    mean', mean*scale
+            print '    Tz  ', Tz
+            print '    ampl', a*scale
+            print '      St', d/(Tz*U0)
+            print '     rms', ((data*scale)**2).mean()**0.5    
+            #pyplot.plot(t[I], data_plot[I]*scale, 'bo')
+            #pyplot.plot(t[J], data_plot[J]*scale, 'ro')
+            print 'Tmax', t[-1]
+            print
+    
+    lines, names = zip(*legends)
+    pyplot.legend(lines, names)
+    pyplot.tight_layout()
+    
+    pyplot.show()
+
+
 if __name__ == '__main__':
-    main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('logfiles', default=['cylinder.log'], nargs='*')
+    parser.add_argument('--tstart', type=float, default=0, help='steady state start')
+    args = parser.parse_args()
+    
+    
+    pyplot.style.use('ggplot')
+    tstart = args.tstart
+    inpfiles = args.logfiles
+    
+    if len(inpfiles) == 1:
+        plot_logfile(inpfiles[0], tstart)
+    else:
+        plot_multiple(inpfiles, tstart)
+        
